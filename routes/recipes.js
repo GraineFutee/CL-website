1d9b82badad40dab02360bdc7884c4e9927c44c2
Pierre ASDRUBAL <pierro@Pierres-MacBook-Pro.local>
2020-05-16 17:24:20 +0200

    New functionality : research

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,20 +1,27 @@
 const express = require('express')
 const router = express.Router()
 const Recipe = require('../models/recipe')
+const url = require('url')
 
 // Home
 router.get('/', (req, res) => {
-    res.render('recipes/index.html');
+    res.render('recipes/index.html')
 });
-
-module.exports = router
 
 // Getting all
 router.get('/recipes', async (req, res) => {
   try {
-    const recipes = await Recipe.find().then(recipes => {
-      res.render('recipes/show.html', {recipes: recipes});
-    })
+    var searchData = url.parse(req.url, true)
+    searchData = searchData.query
+    if (searchData.searchData === undefined) {
+      const recipes = await Recipe.find().then(recipes => {
+        res.render('recipes/show.html', {recipes: recipes})
+      })
+    } else {
+      const recipes = await Recipe.find({ $or: [{"name" : {$regex : ".*" + searchData.searchData + ".*"}}, {ingredients: {$regex : ".*" + searchData.searchData + ".*"}}]}).then(recipes => {
+        res.render('recipes/show.html', {recipes: recipes, search: searchData.searchData})
+      })
+    }
   } catch (err) {
     res.status(500).json({ message: err.message })
   }
@@ -24,9 +31,9 @@ router.get('/recipes', async (req, res) => {
 
 router.get('/recipes/:id', (req, res) => {
   Recipe.findById(req.params.id).then(recipe => {
-    res.render('recipes/showOne.html', {recipe: recipe});
+    res.render('recipes/showOne.html', {recipe: recipe})
   },
-  err => res.status(500).send(err));
+  err => res.status(500).send(err))
 });
 
 module.exports = router
